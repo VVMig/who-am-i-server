c51bae8839a8d0f845fd45ac9cba4f9271f73d4b
Vladimir Migay <vova.migay123@gmail.com>
2021-07-13 16:48:17 +0300

    Add fields

--- src/models/Room.ts
+++ src/models/Room.ts
@@ -8,6 +8,8 @@ export interface IRoom extends Document {
   participants: IGameUser[];
   maxParticipants: number;
   gameStage: GameStage;
+  nowNaming?: IGameUser;
+  nameSeter?: IGameUser;
 }
 
 const roomSchema: Schema<IRoom> = new Schema({
@@ -27,6 +29,14 @@ const roomSchema: Schema<IRoom> = new Schema({
   gameStage: {
     type: String,
     default: GameStage.WAIT_STAGE,
+  },
+  nowNaming: {
+    type: Schema.Types.ObjectId,
+    ref: 'GameUser',
+  },
+  nameSeter: {
+    type: Schema.Types.ObjectId,
+    ref: 'GameUser',
   },
 });
 
