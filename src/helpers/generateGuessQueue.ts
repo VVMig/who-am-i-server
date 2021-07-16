44a8377c1fd26c81b3b15796dddaed14d668cd8d
Vladimir Migay <vova.migay123@gmail.com>
2021-07-16 16:47:06 +0300

    extract variables

--- src/helpers/generateGuessQueue.ts
+++ src/helpers/generateGuessQueue.ts
@@ -3,7 +3,8 @@ import { IRoom } from '../models';
 
 export const generateGuessQueue = async (room: IRoom) => {
   for await (const participant of room.participants) {
-    const gameUserIndex = room.participants.findIndex(
+    const participants = room.participants;
+    const gameUserIndex = participants.findIndex(
       (findParticipant) => findParticipant.id === participant.id
     );
 
@@ -11,15 +12,18 @@ export const generateGuessQueue = async (room: IRoom) => {
       id: participant._id,
     });
 
+    const nextGameUserIndex = gameUserIndex + 1;
+    const prevGameUserIndex = gameUserIndex - 1;
+
     if (gameUserIndex === 0) {
-      gameUser.namingUser = room.participants[room.participants.length - 1];
-      gameUser.seterUser = room.participants[gameUserIndex + 1];
-    } else if (gameUserIndex === room.participants.length - 1) {
-      gameUser.namingUser = room.participants[gameUserIndex - 1];
-      gameUser.seterUser = room.participants[0];
+      gameUser.namingUser = participants[room.participants.length - 1];
+      gameUser.seterUser = participants[nextGameUserIndex];
+    } else if (gameUserIndex === participants.length - 1) {
+      gameUser.namingUser = participants[prevGameUserIndex];
+      gameUser.seterUser = participants[0];
     } else {
-      gameUser.namingUser = room.participants[gameUserIndex - 1];
-      gameUser.seterUser = room.participants[gameUserIndex + 1];
+      gameUser.namingUser = participants[prevGameUserIndex];
+      gameUser.seterUser = participants[nextGameUserIndex];
     }
 
     participant.namingUser = gameUser.namingUser;
